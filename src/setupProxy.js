1cc6988edbc4f125e603049cfbd4343f972f00e3
seock1000 <tjrcjs0705@gmail.com>
2023-05-24 15:01:28 +0900

    proxy root url 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api',
+        '/',
         createProxyMiddleware({
             target: 'http://gcu-metaverse.shop:8080',
             changeOrigin: true,
